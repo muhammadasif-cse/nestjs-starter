5690c609794be2c32e6983e605194a13a34280a0
muhammadasif-cse <muhammadasif.cse@gmail.com>
2025-03-05 20:49:08 +0600

    add Swagger documentation and configure TypeORM with environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,32 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('DB_HOST'),
+        port: +configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        entities: [join(__dirname, '**', '*.entity.{ts,js}')],
+        synchronize: false,
+        migrations: [join(__dirname, 'migrations', '*.{ts,js}')],
+        cli: {
+          migrationsDir: 'src/migrations',
+        },
+        namingStrategy:
+          new (require('typeorm-naming-strategies').SnakeNamingStrategy)(),
+      }),
+      inject: [ConfigService],
+    }),
+    // Other modules
+  ],
 })
 export class AppModule {}
